2775c5798442e3578c3bab7f8b8ab28552e040ed
Emin-G <dev.mingowo@gmail.com>
2021-11-13 18:16:46 +0900

    init

--- test/test.js
+++ test/test.js
@@ -4,4 +4,12 @@ caitlyn.getSummoner("", "", (data) => {
     console.log(data);
 });
 
+caitlyn.ddragon.version((ddver) => {
+    console.log(ddver);
+});
+
+caitlyn.ddragon.queueInfo(440, (queue) => {
+    console.log(queue);
+});
+
 setTimeout(()=>{}, 10000);
